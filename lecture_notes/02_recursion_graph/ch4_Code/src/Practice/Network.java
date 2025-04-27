835cccc0cffd43f7ddccdbe7dd5c30100cbe3066
DESKTOP-DAHYUN\dahye <dah9902@naver.com>
2025-04-27 19:36:09 +0900

    실습 - 네트워크

--- lecture_notes/02_recursion_graph/ch4_Code/src/Practice/Network.java
+++ lecture_notes/02_recursion_graph/ch4_Code/src/Practice/Network.java
@@ -2,17 +2,36 @@ package Practice;
 
 public class Network {
 
-    public int solution(int n, int[][] computers) {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] computers = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}
+        };
 
-        int answer = 0;
+        int count = 0;
+        boolean[] visited = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
-            if (visited == false) {
-                    DFS(i); // BFS(i)
-                answer++;
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(n, computers, visited, i);
+                count++;
             }
         }
 
-        return answer;
+        System.out.println("네트워크의 개수 : " + count);
     }
+
+    public static void dfs(int n, int[][] computers, boolean[] visited, int cur) {
+        visited[cur] = true;
+
+        for(int i = 0; i < n; i++) {
+            if(!visited[i] && computers[cur][i] == 1) {
+                dfs(n, computers, visited, i);
+            }
+        }
+
+    }
+
 }

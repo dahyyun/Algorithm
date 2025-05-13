45e94cbabd4a1492cf4e765afb1c351232277c72
Jung Dahyun <dah8120@gmail.com>
2025-05-13 17:17:36 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bf.readLine().trim();
+        String[] parts = input.split(" ");
+
+        int result = Integer.parseInt(parts[0]) * Integer.parseInt(parts[1]);
+
+        System.out.println(result);
+    }
+}

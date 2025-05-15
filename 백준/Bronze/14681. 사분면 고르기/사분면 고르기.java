fe4b3a5987c4e773a090af674a39c9cb13d53190
Jung Dahyun <dah8120@gmail.com>
2025-05-15 23:00:56 +0900

    [Bronze V] Title: 사분면 고르기, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputx = bf.readLine().trim();
+        int x = Integer.parseInt(inputx);
+
+        String inputy = bf.readLine().trim();
+        int y = Integer.parseInt(inputy);
+
+        if(x > 0) {
+            if (y > 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("4");
+            }
+        } else {
+            if (y > 0) {
+                System.out.println("2");
+            } else {
+                System.out.println("3");
+            }
+        }
+
+
+    }
+}

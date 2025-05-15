75fbb542b5c79ac11a81716acde363e9db4a25cb
Jung Dahyun <dah8120@gmail.com>
2025-05-15 22:53:07 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bf.readLine().trim();
+
+        int score = Integer.parseInt(input);
+
+        if (score >= 90 && score <= 100) {
+            System.out.println("A");
+        } else if (score >= 80 && score <= 89) {
+            System.out.println("B");
+        } else if (score >= 70 && score <= 79) {
+            System.out.println("C");
+        } else if (score >= 60 && score <= 69) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+
+
+
+    }
+}

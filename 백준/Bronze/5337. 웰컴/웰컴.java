9e7cf75cd487c90a7ea4a38e790cd19ff9c5e8a2
Jung Dahyun <dah8120@gmail.com>
2025-05-28 16:44:16 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(".  .   .");
+        bw.write("\n");
+        bw.write("|  | _ | _. _ ._ _  _");
+        bw.write("\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/.");
+
+        bw.flush();
+        bw.close();
+
+    }
+}

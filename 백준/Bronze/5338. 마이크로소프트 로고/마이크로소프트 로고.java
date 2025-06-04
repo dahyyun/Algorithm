4e1da0ca383fd9d515a46195d20d587c46a05d45
Jung Dahyun <dah8120@gmail.com>
2025-06-04 17:45:01 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("       _.-;;-._");
+        bw.write("\n");
+        bw.write("'-..-'|   ||   |");
+        bw.write("\n");
+        bw.write("'-..-'|_.-;;-._|");
+        bw.write("\n");
+        bw.write("'-..-'|   ||   |");
+        bw.write("\n");
+        bw.write("'-..-'|_.-''-._|");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
